c7d78c8f7d8d082f255df4325abd1f3e69742096
laurenthubert <laurenthubert59130@hotmail.com>
2023-12-10 08:29:44 +0100

    modif soutenance

--- src/components/Cards/cards.jsx
+++ src/components/Cards/cards.jsx
@@ -6,6 +6,7 @@ const Cards = ({ id, cover, title }) => (
   <Link to={`/logements/${id}`} className='cards'>
     <figure className='cards__cont'>
       <img className='cards__img' src={cover} alt="" />
+      <div className='overlay'></div>
       <figcaption className='cards__title'>{title}</figcaption>
     </figure>
   </Link>
